29623b3de47ed4ae7f66f0dd45654d7c97e60adf
Jason Zuidijk <jason@Jasons-MBP-3.lan>
2025-05-22 18:48:12 +0100

    showing list of all comments

--- src/api.js
+++ src/api.js
@@ -5,11 +5,21 @@ const NewsBaseApi = axios.create({
 });
 
 export const getArticles = (articleId) => {
-    return NewsBaseApi.get("/articles", { params: {article_id: articleId}}).then((response) => {
-      return response.data.articles;
-    });
-  };
+  return NewsBaseApi.get("/articles", {
+    params: { article_id: articleId },
+  }).then((response) => {
+    return response.data.articles;
+  });
+};
 
-  export const getArticleById = (articleId) => {
-    return NewsBaseApi.get(`/articles/${articleId}`).then((response) => response.data.article);
-  };
+export const getArticleById = (articleId) => {
+  return NewsBaseApi.get(`/articles/${articleId}`).then((response) => {
+    return response.data.article;
+  });
+};
+
+export const getCommentsByArticleId = (articleId) => {
+  return NewsBaseApi.get(`/articles/${articleId}/comments`).then((response) => {
+    return response.data.comments;
+  });
+};
